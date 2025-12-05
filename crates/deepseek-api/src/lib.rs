f5290d4f368a94f4fe59eb9cf26375718392947e
mirus <lomirus.dev@protonmail.com>
2025-12-05 22:42:11 +0800

    Add more chat request fields

--- crates/deepseek-api/src/lib.rs
+++ crates/deepseek-api/src/lib.rs
@@ -38,6 +38,29 @@ pub struct Client {
     pub model: Model,
     pub thinking: Option<Thinking>,
     pub api_key: String,
+
+    /// Number between -2.0 and 2.0. Positive values penalize new tokens based on their existing frequency in the text so far, decreasing the model's likelihood to repeat the same line verbatim.
+    pub frequency_penalty: Option<f32>,
+
+    /// The maximum number of tokens that can be generated in the chat completion.
+    ///
+    /// The total length of input tokens and generated tokens is limited by the model's context length.
+    ///
+    /// For the value range and default value, please refer to the [documentation](https://api-docs.deepseek.com/quick_start/pricing).
+    pub max_tokens: Option<u32>,
+
+    /// Number between -2.0 and 2.0. Positive values penalize new tokens based on whether they appear in the text so far, increasing the model's likelihood to talk about new topics.
+    pub presence_penalty: Option<f32>,
+
+    /// What sampling temperature to use, between 0 and 2. Higher values like 0.8 will make the output more random, while lower values like 0.2 will make it more focused and deterministic.
+    ///
+    /// We generally recommend altering this or `top_p` but not both.
+    pub temperature: Option<f32>,
+
+    /// An alternative to sampling with temperature, called nucleus sampling, where the model considers the results of the tokens with top_p probability mass. So 0.1 means only the tokens comprising the top 10% probability mass are considered.
+    ///
+    /// We generally recommend altering this or `temperature`` but not both.
+    pub top_p: Option<f32>,
 }
 
 impl Client {
@@ -46,6 +69,11 @@ impl Client {
             model,
             thinking: None,
             api_key: api_key.to_string(),
+            frequency_penalty: None,
+            max_tokens: None,
+            presence_penalty: None,
+            temperature: None,
+            top_p: None,
         }
     }
 
@@ -63,6 +91,11 @@ impl Client {
                     messages,
                     thinking: self.thinking.clone(),
                     stream: false,
+                    frequency_penalty: self.frequency_penalty,
+                    max_tokens: self.max_tokens,
+                    presence_penalty: self.presence_penalty,
+                    temperature: self.temperature,
+                    top_p: self.top_p,
                 })
                 .unwrap(),
             )
@@ -85,6 +118,11 @@ impl Client {
                     messages,
                     thinking: self.thinking.clone(),
                     stream: true,
+                    frequency_penalty: self.frequency_penalty,
+                    max_tokens: self.max_tokens,
+                    presence_penalty: self.presence_penalty,
+                    temperature: self.temperature,
+                    top_p: self.top_p,
                 })
                 .unwrap(),
             )
--- crates/deepseek-api/src/request.rs
+++ crates/deepseek-api/src/request.rs
@@ -8,6 +8,29 @@ pub struct ChatCompletionRequest {
     pub model: Model,
     pub thinking: Option<Thinking>,
     pub stream: bool,
+
+    /// Number between -2.0 and 2.0. Positive values penalize new tokens based on their existing frequency in the text so far, decreasing the model's likelihood to repeat the same line verbatim.
+    pub frequency_penalty: Option<f32>,
+
+    /// The maximum number of tokens that can be generated in the chat completion.
+    ///
+    /// The total length of input tokens and generated tokens is limited by the model's context length.
+    ///
+    /// For the value range and default value, please refer to the [documentation](https://api-docs.deepseek.com/quick_start/pricing).
+    pub max_tokens: Option<u32>,
+
+    /// Number between -2.0 and 2.0. Positive values penalize new tokens based on whether they appear in the text so far, increasing the model's likelihood to talk about new topics.
+    pub presence_penalty: Option<f32>,
+
+    /// What sampling temperature to use, between 0 and 2. Higher values like 0.8 will make the output more random, while lower values like 0.2 will make it more focused and deterministic.
+    ///
+    /// We generally recommend altering this or `top_p` but not both.
+    pub temperature: Option<f32>,
+
+    /// An alternative to sampling with temperature, called nucleus sampling, where the model considers the results of the tokens with top_p probability mass. So 0.1 means only the tokens comprising the top 10% probability mass are considered.
+    ///
+    /// We generally recommend altering this or `temperature`` but not both.
+    pub top_p: Option<f32>,
 }
 
 #[derive(Serialize, Deserialize, Clone, Debug)]
